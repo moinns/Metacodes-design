d7d18c44adb1ed83d6f40138a407bb63da7af304
Vitalik <witalik.logoyda@gmail.com>
2022-11-16 17:06:47 +0200

    updat

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ new Swiper('.image-slider', {
 });
 
 $(document).ready(function () {
+  $('.modal-wrapper').css('display', 'none');
+
   $('.content .boxHead').on('click', accordeon);
   $('.burger').on('click', function () {
     $(this).toggleClass('active');
@@ -21,11 +23,46 @@ $(document).ready(function () {
     $('.navitems').toggleClass('active');
     $('body').toggleClass('lock');
   });
+
   $('.nav *').on('click', function () {
     $('.nav').removeClass('active');
     $('.navitems').removeClass('active');
     $('body').removeClass('lock');
     $('.burger').removeClass('active');
+  });
+
+  $('#acc').on('click', () => {
+    var container = $('.modal-wrapper');
+    container.toggleClass('show');
+    container.css('display', 'block');
+    if (!container.hasClass('show')) {
+      setTimeout(() => {
+        container.css('display', 'none');
+      }, 300);
+    }
+  });
+
+  $('#position-view').on('click', () => {
+    var container = $('.modal-position');
+    container.toggleClass('show');
+    $('.dark').addClass('active');
+    $('body').css('overflow', 'hidden');
+    if (!container.hasClass('show')) {
+      setTimeout(() => {
+        container.css('display', 'none');
+      }, 300);
+    }
+  });
+
+  $(document).mouseup(function (e) {
+    if ($('.modal-wrapper').has(e.target).length === 0) {
+      $('.modal-wrapper').removeClass('show');
+    }
+    if ($('.modal-position').has(e.target).length === 0) {
+      $('.modal-position').removeClass('show');
+      $('.dark').removeClass('active');
+      $('body').css('overflow', 'auto');
+    }
   });
 });
 
